4c80ca6b02d9affc141808bcf610f3ef449a4c21
Armand <armandchun@gmail.com>
2019-08-03 14:28:48 -0400

    add basic etch-a-sketch

---
+++ javascript.js
@@ -0,0 +1,22 @@
+let container = document.querySelector('.container');
+
+function createGrid (size) {
+  for (let i = 0; i < size; i++) {
+    let box = document.createElement('div');
+    box.id = 'box' + i;
+    box.classList.add('box');
+    box.addEventListener('mouseenter', changeBox);
+    container.appendChild(box);
+  }
+}
+
+function changeBox (e){
+  e.target.style.backgroundColor = 'black';
+}
+
+function resetBoxes (e){
+  let allBoxes = container.querySelectorAll('.box');
+  allBoxes.forEach();
+}
+
+createGrid(256);
